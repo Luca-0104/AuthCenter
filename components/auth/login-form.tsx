a77b935a93f0dbaf8781d120f1ea83507d419dad
Liu Zhe <lzhe72537@gmail.com>
2024-05-26 18:16:00 -0400

    submit login form server actions

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import { CardWrapper } from './card-wrapper'
 import * as z from "zod";
 import { LoginSchema } from '@/schemas';
@@ -11,8 +11,13 @@ import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { FormError } from '../form-error';
 import { FormSuccess } from '../form-success';
+import { login } from '@/actions/login';
+import { useTransition } from 'react';
 
 export const LoginForm = () => {
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
@@ -21,9 +26,17 @@ export const LoginForm = () => {
     }
   });
 
+
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-    console.log(values);
-    
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      login(values).then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      })
+    });
   };
 
   return (
@@ -33,35 +46,35 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
-              <div className='space-y-4'>
-                <FormField control={form.control} name="email" render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder='youremail@gmail.com' type='email' />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
-                <FormField control={form.control} name="password" render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="******" type='password' />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
-              </div>
-              <FormError message='This is an error' />
-              <FormSuccess message='This is a success' />
-              <Button type='submit' className='w-full'>
-                Login
-              </Button>
-            </form>
-        </Form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+          <div className='space-y-4'>
+            <FormField control={form.control} name="email" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder='youremail@gmail.com' type='email' disabled={isPending}/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )} />
+            <FormField control={form.control} name="password" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="******" type='password' disabled={isPending}/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )} />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type='submit' className='w-full' disabled={isPending}>
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   )
 }
