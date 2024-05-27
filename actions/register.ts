559eece33eb5e0e2bf4ea5ddec5f0cd1fa6c945d
Liu Zhe <lzhe72537@gmail.com>
2024-05-26 23:26:44 -0400

    extract the user operation into data layer

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcrypt";
 import * as z from "zod";
 import { db } from "@/lib/db";
+import {getUserByEmail} from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -15,12 +16,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const {email, password, name} = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    }
-  });
-
+  const existingUser = await getUserByEmail(email);
   if (existingUser) {
     return {error: "Email already in use!"};
   }
