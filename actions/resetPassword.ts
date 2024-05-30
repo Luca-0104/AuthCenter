f8d06aaab3f3a1dbd049178240a7e817adb37b0d
Liu Zhe <lzhe72537@gmail.com>
2024-05-29 22:48:08 -0400

    new prisma model schema for PasswordResetToken

--- actions/resetPassword.ts
+++ actions/resetPassword.ts
@@ -1,8 +1,26 @@
 "use server";
 
+import { getUserByEmail } from "@/data/user";
 import { ResetSchema } from "@/schemas";
 import * as z from "zod";
 
 export const resetPassword = async (values: z.infer<typeof ResetSchema>) => {
+  const validatedFields = ResetSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return {error: "Invalid email!"};
+  }
+
+  const {email} = validatedFields.data;
+
+  // check the email
+  const existingUser = await getUserByEmail(email);
+  if (!existingUser || !existingUser.email) return {error: "Email does not exist!"};
+  if (!existingUser.password) return {error: "This email was used with OAuth providers! (Not registered with credentials)"};
+
+  // generate password reset token
+
+  // send the email with token
+  
 
 }
