b0d1ceac939f84de49aea82f12088b1cf997c130
Liu Zhe <lzhe72537@gmail.com>
2024-06-02 02:51:50 -0400

    callback for the OAuth login

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -6,11 +6,15 @@ import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { signIn } from 'next-auth/react';
+import { useSearchParams } from 'next/navigation';
 
 export const Social = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+  
   const onClick = (provider: "google" | "github") => {
     signIn(provider, {
-      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+      callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
     })
   };
 
