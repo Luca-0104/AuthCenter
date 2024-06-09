7f690977f193493db795ba85ebc28a815c4867a1
Liu Zhe <lzhe72537@gmail.com>
2024-06-09 02:24:47 -0400

    update

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -3,7 +3,7 @@
 import { setting } from "@/actions/settings";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { SettingsSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useSession } from "next-auth/react";
@@ -14,6 +14,15 @@ import * as z from "zod";
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { FormSuccess } from "@/components/form-success";
 import { FormError } from "@/components/form-error";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { UserRole } from "@prisma/client";
+
 
 const Settings = () => {
   const user = useCurrentUser();
@@ -21,7 +30,7 @@ const Settings = () => {
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
 
-  const {update} = useSession();
+  const { update } = useSession();
   const [isPending, startTransition] = useTransition();
 
   const form = useForm<z.infer<typeof SettingsSchema>>({
@@ -31,9 +40,10 @@ const Settings = () => {
       email: user?.email || undefined,
       password: undefined,
       newPassword: undefined,
+      role: user?.role || undefined,
     },
   });
-  
+
   const handleOnSubmit = (values: z.infer<typeof SettingsSchema>) => {
     startTransition(() => {
       setError("");
@@ -67,32 +77,56 @@ const Settings = () => {
               <FormItem>
                 <FormLabel>Name</FormLabel>
                 <FormControl>
-                  <Input {...field} placeholder="The username" disabled={isPending}/>
+                  <Input {...field} placeholder="The username" disabled={isPending} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )} />
             <FormField control={form.control} name="email" render={({ field }) => (
               <FormItem>
                 <FormLabel>Email</FormLabel>
                 <FormControl>
-                  <Input {...field} placeholder="Email" disabled={isPending}/>
+                  <Input {...field} placeholder="Email" disabled={isPending} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )} />
             <FormField control={form.control} name="password" render={({ field }) => (
               <FormItem>
                 <FormLabel>Password</FormLabel>
                 <FormControl>
-                  <Input {...field} placeholder="******" disabled={isPending}/>
+                  <Input {...field} placeholder="******" disabled={isPending} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )} />
             <FormField control={form.control} name="newPassword" render={({ field }) => (
               <FormItem>
                 <FormLabel>New Password</FormLabel>
                 <FormControl>
-                  <Input {...field} placeholder="******" disabled={isPending}/>
+                  <Input {...field} placeholder="******" disabled={isPending} />
                 </FormControl>
+                <FormMessage />
+              </FormItem>
+            )} />
+            <FormField control={form.control} name="role" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Role</FormLabel>
+                <FormControl>
+                  <Select 
+                    disabled={isPending} 
+                    onValueChange={field.onChange} 
+                    defaultValue={field.value}>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select a role" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value={UserRole.USER}>User</SelectItem>
+                      <SelectItem value={UserRole.ADMIN}>Admin</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
               </FormItem>
             )} />
             <FormSuccess message={success} />
