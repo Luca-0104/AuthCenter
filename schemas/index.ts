5986f870c3c5925ab86afde4980c5a4d816c7890
Liu Zhe <lzhe72537@gmail.com>
2024-06-08 23:56:01 -0400

    finish the SettingsSchema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,31 @@
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
 
 export const SettingsSchema = z.object({
   name: z.optional(z.string()),
+  isTwoFactorEnabled: z.optional(z.boolean()),
+  role: z.enum([UserRole.USER, UserRole.ADMIN]),
+  email: z.optional(z.string().email()),
+  password: z.optional(z.string().min(6)),
+  newPassword: z.optional(z.string().min(6)),
+
+}).refine((data) => {
+  if (data.password && !data.newPassword) {
+    return false;
+  }
+  return true;
+}, {
+  message: "New password is required!",
+  path: ["newPassword"],
+  
+}).refine((data) => {
+  if (data.newPassword && !data.password) {
+    return false;
+  }
+  return true;
+}, {
+  message: "Old password is required!",
+  path: ["password"],
 })
 
 export const ResetPasswordSchema = z.object({
