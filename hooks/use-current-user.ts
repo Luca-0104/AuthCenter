c4252e54aec7a4339c16b879a43eddd55279f96d
Liu Zhe <lzhe72537@gmail.com>
2024-06-10 01:37:12 -0400

    update

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -7,6 +7,7 @@ export const useCurrentUser = () => {
   // This ! is ! so ! fucking ! weird!!!!
   // If we do not set `{ required: true }`, the useSession cannot get anything after logging in, until refresh!!!!
   // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
-  const session = useSession({ required: true });
+  // const session = useSession({ required: true });
+  const session = useSession();
   return session.data?.user;
 };
